3d595f8f33e660153005c10ebc3b6b8eca132dce
ivnmrtk <ivnmrtk@gmail.com>
2023-12-04 00:59:14 +0300

    day1 - second half implemented

--- src/main/kotlin/com/github/ivnmrtk/day01/Day01.kt
+++ src/main/kotlin/com/github/ivnmrtk/day01/Day01.kt
@@ -1,10 +1,21 @@
 package com.github.ivnmrtk.day01
 
 import java.io.File
+import java.util.*
+import java.util.regex.Pattern
 
 fun main() {
-    var sum: Int = 0
-    File("src/main/kotlin/com/github/ivnmrtk/day01/input.txt").forEachLine {
+    val inputFile = File("src/main/kotlin/com/github/ivnmrtk/day01/input.txt")
+    val firstSum = getSumOnlyByDigits(inputFile)
+    println("I approach sum is $firstSum\n")
+    val secondSum = getActualSum(inputFile)
+    println("II approach sum is $secondSum")
+}
+
+fun getSumOnlyByDigits(file: File): Int {
+    println("I approach calculation started")
+    var sum = 0
+    file.forEachLine {
         var isDigitEncountered = false
         val numStringBuilder = StringBuilder()
         var lastEncounteredDigit = ' '
@@ -26,5 +37,42 @@ fun main() {
             }
         }
     }
-    println("Final sum is $sum")
+    return sum
+}
+
+fun getActualSum(file: File): Int {
+    println("II approach calculation started")
+    var sum = 0
+    file.forEachLine { line ->
+        val regex = "(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))."
+        val digitsWordsDict = mapOf(
+            "one" to "1",
+            "two" to "2",
+            "three" to "3",
+            "four" to "4",
+            "five" to "5",
+            "six" to "6",
+            "seven" to "7",
+            "eight" to "8",
+            "nine" to "9"
+        )
+        val digitToIndex = TreeMap<Int, String>()
+        val pattern = Pattern.compile(regex)
+        val matcher = pattern.matcher(line)
+        while (matcher.find()) {
+            val key = matcher.start()
+            val value = matcher.group(1).let {
+                return@let if (it.matches("\\d".toRegex())) {
+                    it
+                } else {
+                    digitsWordsDict[it]
+                }
+            }
+            digitToIndex[key] = value!!
+        }
+        val extractedNumber = "${digitToIndex.firstEntry().value}${digitToIndex.lastEntry().value}".toInt()
+        sum += extractedNumber
+        println("For string $line result is $extractedNumber, sum is $sum")
+    }
+    return sum
 }
